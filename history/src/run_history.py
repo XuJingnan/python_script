13ecf527413a3f223aa08660dbf374d8dbc860ae
xujingnan <xujingnan308@163.com>
2016-02-20 00:42:54 +0800

    update run history script

--- history/src/run_history.py
+++ history/src/run_history.py
@@ -1,57 +1,61 @@
-import commands
 import datetime
 import sys
+import threading
+
 import time
 
-KEY_TASK_NAME = "task_name"
-KEY_TASK_OBJ = "task_obj"
-KEY_PARA1 = "para1"
-KEY_CYCLE = "cycle"
-SUCCESS = 0
-TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-
-def init():
-    tasks = {}
-    with open("mysql_task_conf.csv") as f:
-        for line in f:
-            task_id, task_obj, para1, cycle = line.strip().split(",")
-            tasks[task_id] = {KEY_TASK_OBJ: task_obj, KEY_PARA1: para1, KEY_CYCLE: cycle}
-    return tasks
-
-
-def get_timestamp(time_str, format):
-    return int(time.mktime(datetime.datetime.strptime(time_str, format).timetuple()))
-
-
-def run(argv, tasks):
-    task_id = argv[0]
-    configurations = tasks[task_id]
-    start = get_timestamp(argv[1], TIME_FORMAT)
-    end = get_timestamp(argv[2], TIME_FORMAT)
-    if configurations[KEY_CYCLE] == 'D':
+def generate_all_timestamp(cycle, start_timestamp, end_timestamp):
+    all_timestamps = []
+    start = start_timestamp
+    step = 0
+    if cycle == "D":
         step = 24 * 60 * 60
-        all_times = range(start, end + step, step)
-    else:
+    elif cycle == "H":
         step = 60 * 60
-        all_times = range(start, end + step, step)
-    for t in all_times:
-        cmd1 = configurations[KEY_TASK_OBJ].replace('"', '')
-        cmd2 = configurations[KEY_PARA1].replace("${unix_timestamp}", str(t)).replace('"', "'")
-        cmd = cmd1 + ' ' + cmd2
-        print cmd
-        a, b = commands.getstatusoutput(cmd)
-        if a != SUCCESS:
-            print b
-            with open("fail.tasks", "a") as f:
-                f.write(cmd + "\n")
-        else:
-            print b
-            with open("success.tasks", "a") as f:
-                f.write(cmd + "\n")
+    while start <= end_timestamp:
+        all_timestamps.append(start)
+        start += step
+    return all_timestamps
 
 
-# python run_history.py task_id start_time end_time
+def generate_all_parameters(script_parameters, all_timestamps):
+    all_parameters = []
+    for t in all_timestamps:
+        all_parameters.append(script_parameters.replace("timestamps", str(t)).split(" "))
+    return all_parameters
+
+
+def batch_process(threads):
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+
+def generate_cmd(args):
+    script_name = args[0]
+    module = __import__(script_name)
+    methed = getattr(module, args[1])
+    cycle = args[2]
+    start_timestamps = int(time.mktime(datetime.datetime.strptime(args[3], "%Y-%m-%d %H:%M:%S").timetuple()))
+    end_timestamps = int(time.mktime(datetime.datetime.strptime(args[4], "%Y-%m-%d %H:%M:%S").timetuple()))
+    parallel = int(args[5])
+    script_parameters = " ".join(args[6:])
+    all_timestamps = generate_all_timestamp(cycle, start_timestamps, end_timestamps)
+    all_parameters = generate_all_parameters(script_parameters, all_timestamps)
+
+    threads = []
+    cnt = 0
+    for p in all_parameters:
+        cnt += 1
+        threads.append(threading.Thread(target=methed, args=tuple(p), ))
+        if cnt % parallel == 0:
+            batch_process(threads)
+            threads = []
+    batch_process(threads)
+
+
+# python run_history.py script_name exec_func cycle start_time end_time script_parameters
 if __name__ == "__main__":
-    tasks = init()
-    run(sys.argv[1:], tasks)
+    generate_cmd(sys.argv[1:])

399e3cad336f5f40d67fbb9950037e8881fa4d7b
xujingnan <xujingnan308@163.com>
2016-03-02 11:35:06 +0800

    1. no log check 2. concurrence execute 3. 3 days buffer

--- solar_upload_logs_2_s3_hourly/src/upload_logs_2_s3_hourly.py
+++ solar_upload_logs_2_s3_hourly/src/upload_logs_2_s3_hourly.py
@@ -1,7 +1,9 @@
 import datetime
-import os.path
 import subprocess
 import sys
+import threading
+
+import queue
 
 SUCCESS = 0
 ERROR_CHECK_PARAMETERS = 1
@@ -10,13 +12,12 @@ ERROR_CHECK_S3_FILE = 3
 ERROR_RM_S3_FILE = 4
 ERROR_UPLOAD_FILE = 5
 ERROR_RM_SERVER_FILE = 6
-SUCCESS_DONE = 7
 
 
 def exe_cmd(cmd):
     try:
         b = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-        return 0, b
+        return 0, b.decode('gb2312')
     except subprocess.CalledProcessError as e:
         return e.returncode, e.output
 
@@ -28,82 +29,92 @@ def parameters_check(argv):
     return SUCCESS
 
 
-def get_logs(day, hour):
-    ls_cmd = '''dir *.%s.%s | find "log_"''' % (day, hour)
+def get_logs(day):
+    logs = []
+
+    yesterday = (datetime.datetime.strptime(day, "%Y%m%d") + datetime.timedelta(days=-1)).strftime('%Y%m%d')
+    the_day_before_yesterday = (datetime.datetime.strptime(day, "%Y%m%d") + datetime.timedelta(days=-2)).strftime(
+        '%Y%m%d')
+
+    ls_cmd = 'dir log_*.{day}.* | find "log_"'.format(day=day)
     a, b = exe_cmd(ls_cmd)
     if a == 0:
-        return SUCCESS, b
-    elif os.path.exists("%s.%s.done" % (day, hour)):
-        return SUCCESS_DONE, b
+        for log in b.split('\r\n'):
+            logs.append(log.split(' ')[-1])
     else:
-        print("none logs for %s.%s" % (day, hour))
-        return ERROR_GET_LOGS, None
+        print("none logs for {day}".format(day=day))
 
-
-def s3_file_check(dest_path):
-    test_cmd = "aws s3 ls %s" % dest_path
-    a, b = exe_cmd(test_cmd)
+    ls_cmd = 'dir log_*.{day}.* | find "log_"'.format(day=yesterday)
+    a, b = exe_cmd(ls_cmd)
     if a == 0:
-        print("s3 file check:\t%s exists." % dest_path)
-        return ERROR_CHECK_S3_FILE
+        for log in b.split('\r\n'):
+            logs.append(log.split(' ')[-1])
     else:
-        return SUCCESS
+        print("none logs for {day}".format(day=yesterday))
+
+    ls_cmd = 'dir log_*.{day}.* | find "log_"'.format(day=the_day_before_yesterday)
+    a, b = exe_cmd(ls_cmd)
+    if a == 0:
+        for log in b.split('\r\n'):
+            logs.append(log.split(' ')[-1])
+    else:
+        print("none logs for {day}".format(day=the_day_before_yesterday))
+
+    logs = [log for log in logs if log != '']
+    if len(logs) > 0:
+        return SUCCESS, logs
+    else:
+        return ERROR_GET_LOGS, None
 
 
 def rm_s3_file(dest_path):
     rm_cmd = "aws s3 rm %s" % dest_path
-    a, b = exe_cmd(rm_cmd)
-    if a != 0:
-        print("rm s3 file %s error!" % dest_path)
-        print("error log\n%s" % b)
-        return ERROR_RM_S3_FILE
-    else:
-        print("rm s3 file %s success!" % dest_path)
-        return SUCCESS
+    exe_cmd(rm_cmd)
 
 
 def upload_file(file_name, dest_path):
     upload_cmd = "aws s3 cp %s %s" % (file_name, dest_path)
     a, b = exe_cmd(upload_cmd)
     if a == 0:
-        print("upload file %s to %s success!" % (file_name, dest_path))
-        return SUCCESS
+        return SUCCESS, None
     else:
-        print("cmd:%s\n" % upload_cmd)
-        print("return code:%s\n" % a)
-        print("error log:\n%s" % b)
-        print("upload file %s to %s fail!" % (file_name, dest_path))
-        return ERROR_UPLOAD_FILE
+        return ERROR_UPLOAD_FILE, "upload file {file} error: {msg}".format(file=file_name, msg=b)
 
 
 def rm_server_file(file_name, day, hour):
     a, b = exe_cmd("rm %s" % file_name)
     if a != 0:
-        print("rm server file %s error!" % file_name)
-        print("error log\n%s" % b)
-        return ERROR_RM_SERVER_FILE
-    else:
-        print("rm server file %s success!" % file_name)
+        return ERROR_RM_SERVER_FILE, "rm server file {file} error.".format(file=file_name)
     with open("%s.%s.done" % (day, hour), "a") as f:
-        f.write("%s has uploaded!\n" % file_name)
-    return SUCCESS
+        f.write("%s: %s has uploaded!\n" % (str(datetime.datetime.now()), file_name))
+    the_day_last_month = (datetime.datetime.strptime(day, "%Y%m%d") + datetime.timedelta(days=-31)).strftime('%Y%m%d')
+    rm_cmd = "rm {day}.{hour}.done".format(day=the_day_last_month, hour=hour)
+    exe_cmd(rm_cmd)
+    return SUCCESS, None
 
 
 def upload_log_2_s3(file_name):
     log_type, site_id, day, hour = file_name.split(".")
     dest_path = "s3://ocean/Log/solar/%s/site=%s/yyyymmdd=%s/%s" % (log_type, site_id, day, file_name)
-    res = s3_file_check(dest_path)
-    if res != SUCCESS:
-        res = rm_s3_file(dest_path)
-        if res != SUCCESS:
-            return res
+    rm_s3_file(dest_path)
 
-    res = upload_file(file_name, dest_path)
+    res, msg = upload_file(file_name, dest_path)
     if res != SUCCESS:
-        return res
+        q.put((file_name, res, msg))
+        return
 
-    res = rm_server_file(file_name, day, hour)
-    return res
+    res, msg = rm_server_file(file_name, day, hour)
+    q.put((file_name, res, msg))
+
+
+q = queue.Queue()
+
+
+def batch_process(threads):
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
 
 def upload_logs_2_s3(argv):
@@ -114,26 +125,34 @@ def upload_logs_2_s3(argv):
     day, hour = (datetime.datetime.fromtimestamp(int(argv[0])) - datetime.timedelta(hours=9)).strftime(
         '%Y%m%d %H').split(' ')
 
-    res, logs = get_logs(day, hour)
-    if res == SUCCESS_DONE:
-        print("has done %s.%s before" % (day, hour))
+    res, logs = get_logs(day)
+    if res != SUCCESS:
         exit(SUCCESS)
-    elif res == ERROR_GET_LOGS:
-        exit(res)
 
-    logs = [log[log.find('log_'):] for log in logs.decode('gb2312').split('\r\n') if log != '']
+    threads = []
+    cnt = 0
     for log in logs:
-        res = upload_log_2_s3(log)
-        if res != SUCCESS:
-            exit(res)
-    return SUCCESS
+        t = threading.Thread(target=upload_log_2_s3, name='thread-' + log, args=(log,))
+        threads.append(t)
+        cnt += 1
+        if cnt % 20 == 0:
+            batch_process(threads)
+            threads = []
+    batch_process(threads)
+
+    result = list()
+    while not q.empty():
+        result.append(q.get())
+    return_code = 0
+    for item in result:
+        if item[1] != 0:
+            return_code += item[1]
+            print("{path} occurs {error} error".format(path=item[0], error=item[2]))
+    if return_code == 0:
+        print("{day}.{hour} task success!".format(day=day, hour=hour))
+    exit(return_code)
 
 
 # cmd example:   python upload_logs_2_s3_hourly.py 1449536400
 if __name__ == "__main__":
-    res = upload_logs_2_s3(sys.argv[1:])
-    if res == SUCCESS:
-        print("upload logs 2 s3 successfully")
-    else:
-        print("upload logs 2 s3 fail")
-    exit(res)
+    upload_logs_2_s3(sys.argv[1:])

ba0bc9da12e9af2731949131af68ee135291e03e
xujingnan <xujingnan308@163.com>
2016-03-14 16:00:17 +0800

    bug fix

--- history/src/run_history.py
+++ history/src/run_history.py
@@ -36,7 +36,7 @@ def batch_process(threads):
 def generate_cmd(args):
     script_name = args[0]
     module = __import__(script_name)
-    methed = getattr(module, args[1])
+    method = getattr(module, args[1])
     cycle = args[2]
     start_timestamps = int(time.mktime(datetime.datetime.strptime(args[3], "%Y-%m-%d %H:%M:%S").timetuple()))
     end_timestamps = int(time.mktime(datetime.datetime.strptime(args[4], "%Y-%m-%d %H:%M:%S").timetuple()))
@@ -49,13 +49,13 @@ def generate_cmd(args):
     cnt = 0
     for p in all_parameters:
         cnt += 1
-        threads.append(threading.Thread(target=methed, args=tuple(p), ))
+        threads.append(threading.Thread(target=method, args=tuple(p), ))
         if cnt % parallel == 0:
             batch_process(threads)
             threads = []
     batch_process(threads)
 
 
-# python run_history.py script_name exec_func cycle start_time end_time script_parameters
+# run_history.py upload_logs_2_s3_hourly_total execute D '2016-03-03 09:00:00' '2016-03-08 09:00:00' 1 timestamps 1
 if __name__ == "__main__":
     generate_cmd(sys.argv[1:])

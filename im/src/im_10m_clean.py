1. calculate by turbine 2. add cal air density field 3. fix ntf calculation bug 4. fix temperature bug 5. add production base logic

im/src/im_10m_clean.py
@@ -1,7 +1,3 @@
-import datetime
-import numpy as np
-import os
-
 from utils import *
 
 
@@ -44,17 +40,18 @@ def production_clean_more(df, key, values):
     # production decrease
     df[TABLE_IM_10M_CLEAN_CLEAN_FLAG][delta < values[0]] = df[TABLE_IM_10M_CLEAN_CLEAN_FLAG].apply(
         lambda x: clean_flag_set(x, key, CLEAN_FLAG_TAG_PRODUCTION_DECREASE))
-    # production too large
+    # production out of max range
     df[TABLE_IM_10M_CLEAN_CLEAN_FLAG][delta > values[1]] = df[TABLE_IM_10M_CLEAN_CLEAN_FLAG].apply(
         lambda x: clean_flag_set(x, key, CLEAN_FLAG_TAG_PRODUCTION_TOO_LARGE))
     return df
 
 
-def clean_data(df):
-    df.sort_index(by=TABLE_TBL_POINTVALUE_10M_DATATIME, inplace=True)
+def clean_data(turbine, df):
+    df = df.sort_index(by=TABLE_TBL_POINTVALUE_10M_DATATIME)
     rules = read_rules('clean_rules')
 
     df[TABLE_TBL_POINTVALUE_10M_NACELLEPOSITIONAVE] %= 360
+    df[TABLE_TBL_POINTVALUE_10M_TEMOUTAVE] += CONSTANT_TEMPERATURE
 
     df[TABLE_IM_10M_CLEAN_CLEAN_FLAG] = pd.Series(np.zeros(len(df.index), dtype=np.int64), index=df.index)
     for key, values in rules.iteritems():
@@ -64,30 +61,31 @@ def clean_data(df):
         # out of range check
         df[TABLE_IM_10M_CLEAN_CLEAN_FLAG][
             (key != TABLE_TBL_POINTVALUE_10M_APPRODUCTION) & ((df[key] < values[0]) | (df[key] > values[1]))] = df[
-            TABLE_IM_10M_CLEAN_CLEAN_FLAG].apply(
-            lambda x: clean_flag_set(x, key, CLEAN_FLAG_TAG_OUT_OF_RANGE))
-
-        # more speed check
+            TABLE_IM_10M_CLEAN_CLEAN_FLAG].apply(lambda x: clean_flag_set(x, key, CLEAN_FLAG_TAG_OUT_OF_RANGE))
+        # more wind speed check
         if key == TABLE_TBL_POINTVALUE_10M_WINDSPEEDAVE:
             df = wind_speed_clean_more(df, key)
         # more production check
         if key == TABLE_TBL_POINTVALUE_10M_APPRODUCTION:
             df = production_clean_more(df, key, values)
-    return df
+
+    write_date(turbine, df)
 
 
-def write_date(df):
-    date_dir = str(datetime.datetime.now().strftime('%Y-%m-%d'))
-    out_dir = os.sep.join([OUTPUT_DIR, date_dir])
+def write_date(turbine, df):
+    date = str(datetime.datetime.now().strftime('%Y-%m-%d'))
+    out_dir = os.sep.join([OUTPUT_DIR, date, TABLE_IM_10M_CLEAN])
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    df.to_csv(os.sep.join([OUTPUT_DIR, date_dir, TABLE_IM_10M_CLEAN]), index=False)
+    df.to_csv(os.sep.join([out_dir, str(turbine)]), index=False)
+    with open(os.sep.join([out_dir, IM_TURBINES]), 'a') as f:
+        f.write(str(turbine) + '\n')
 
 
 def im_10m_clean():
     df = read_data()
-    df = group_process(df.groupby(TABLE_TBL_POINTVALUE_10M_WTG_ID), clean_data)
-    write_date(df)
+    groups = df.groupby(TABLE_TBL_POINTVALUE_10M_WTG_ID)
+    group_process(groups, clean_data)
 
 
 if __name__ == '__main__':